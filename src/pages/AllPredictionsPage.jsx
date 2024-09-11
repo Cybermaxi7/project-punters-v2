f935c6b166d89ce46338f1604f90bffba8a15f09
unknown <daleksklee@gmail.com>
2024-09-11 14:39:33 +0100

    Remove redundant header component in all pages

--- src/pages/AllPredictionsPage.jsx
+++ src/pages/AllPredictionsPage.jsx
@@ -2,16 +2,14 @@ import AllPredictions from "../components/AllPredictions";
 import Blog from "../components/Blog";
 import Hero from "../components/Hero";
 import NewsLetter from "../components/NewsLetter";
-import Header from "../shared/Header";
 
 export default function AllPredictionsPage() {
-    return (
-        <div className=" font-inter text-[14px]">
-            <Header />
-            <Hero />
-            <AllPredictions />
-            <Blog />
-            <NewsLetter />
-        </div>
-    );
+  return (
+    <div className=" font-inter text-[14px]">
+      <Hero />
+      <AllPredictions />
+      <Blog />
+      <NewsLetter />
+    </div>
+  );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,17 +3,15 @@ import Blog from "../components/Blog";
 import Hero from "../components/Hero";
 import PredictionsForYou from "../components/PredictionsForYou";
 import Footer from "../shared/Footer";
-import Header from "../shared/Header";
 
 export default function LandingPage() {
-    return (
-        <div className=" font-inter text-[14px]">
-            <Header />
-            <Hero />
-            <PredictionsForYou />
-            <BestBet />
-            <Blog />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className=" font-inter text-[14px]">
+      <Hero />
+      <PredictionsForYou />
+      <BestBet />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
