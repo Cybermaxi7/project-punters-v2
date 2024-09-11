562336b7cedef9a3df5f9f781dcb9d9b9a64e88e
shamoo53 <aliyushamsu16@gmail.com>
2024-09-11 16:11:02 +0100

    adding the header

--- src/pages/AllPredictionsPage.jsx
+++ src/pages/AllPredictionsPage.jsx
@@ -2,12 +2,10 @@ import AllPredictions from "../components/AllPredictions";
 import Blog from "../components/Blog";
 import Hero from "../components/Hero";
 import NewsLetter from "../components/NewsLetter";
-import Header from "../shared/Header";
 
 export default function AllPredictionsPage() {
     return (
         <div className=" font-inter text-[14px]">
-            <Header />
             <Hero />
             <AllPredictions />
             <Blog />
