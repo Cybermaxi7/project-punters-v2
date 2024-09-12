d820919aa65ee270db051f2c7635275fdf20fc26
unknown <daleksklee@gmail.com>
2024-09-12 16:44:43 +0100

    Removed the blog and footer

--- src/pages/AllPredictionsPage.jsx
+++ src/pages/AllPredictionsPage.jsx
@@ -1,16 +1,11 @@
 import AllPredictions from "../components/AllPredictions";
-import Blog from "../components/Blog";
 import HeroPredictions from "../components/HeroPredictions";
-import NewsLetter from "../components/NewsLetter";
 
 export default function AllPredictionsPage() {
-    return (
-        <div className=" font-inter text-[14px]">
-            <HeroPredictions />
-            <AllPredictions />
-            <Blog />
-            <NewsLetter />
-        </div>
-    );
-
+  return (
+    <div className=" font-inter text-[14px]">
+      <HeroPredictions />
+      <AllPredictions />
+    </div>
+  );
 }
