5d68b16d3ad61d5f81bb1d2b82422d85bd1e5d02
cybermaxi <maximusagbe@gmail.com>
2024-09-13 09:38:20 +0100

    worked on the nested routes and pages for games

--- src/components/FootballLeagues.jsx
+++ src/components/FootballLeagues.jsx
@@ -5,7 +5,7 @@ import { topLeagues } from "../utils/stats";
 
 export default function FootballLeagues() {
   return (
-    <div className="bg-white rounded-[8px] w-full border border-[#DBE1E7] card-stats-shadow font-gilroy mt-6 md:mt-12 lg:mt-16">
+    <div className="bg-white rounded-[8px] w-full border border-[#DBE1E7] card-stats-shadow font-gilroy mt-6 md:mt-12 lg:mt-16 h-fit">
       <div className="flex bg-[#F7F8F9] border-b border-[#DBE1E7] rounded-[8px_8px_0_0] items-center justify-between p-5">
         <h2 className=" font-semibold text-[20px] leading-[30px]">
           Football Leagues
